fbd3da67d93330ac3916632864ebf00dab546193
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2022-11-01 15:52:40 +0530

    Committing files

---
+++ CodeChef/C++14/CRZYSUBSTR/65150946.cpp
@@ -0,0 +1,121 @@
+// Created by: G Gautham Krishna
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// shortforms
+
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define all(x) (x).begin(),(x).end()
+#define srt(v) sort(v.begin(),v.end())
+#define rev(v) reverse(v.begin(),v.end())
+#define lb(v,x) lower_bound(v.begin(),v.end(),x)
+#define ub(v,x) upper_bound(v.begin(),v.end(),x)
+#define cpy(v2,v1) v2.assign(v1.begin(),v1.end())
+#define maxv(a) *max_element(a.begin(), a.end())
+#define minv(a) *min_element(a.begin(), a.end())
+#define ff first
+#define ss second
+
+//type definitions
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+//constants
+
+const long long int inf = 1e18;
+const int mod = 1000000007;
+#define pi 3.141592653589793238462
+
+//graphs
+
+// const int N=2e5+2;
+
+// debugger
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(multimap <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+int main() {
+
+    //fast io
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    //main code starts here
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        vector<priority_queue<int> >v(26);
+        int al=s[0];
+        int cnt=1;
+        int ans1=0;
+        for(int i=1;i<s.size();i++){
+            if(s[i]!=al){
+                v[al-'a'].push(cnt);
+                ans1=max(ans1,cnt);
+                cnt=1;
+                al=s[i];
+            }
+            else{
+                cnt++;
+            }
+        }
+        v[al-'a'].push(cnt);
+        ans1=max(ans1,cnt);
+        // debug(s)
+        cout<<ans1<<" ";
+        for(int i=0;i<n;i++){
+            int m=0;
+            for(int j=0;j<26;j++){
+                // debug(i)
+                // debug(j)
+                if(v[j].size()>=2){
+                    int x=v[j].top();
+                    v[j].pop();
+                    int y=v[j].top();
+                    v[j].pop();
+                    v[j].push(x+y);
+                }
+                if(v[j].size()>0)m=max(m,v[j].top());
+            }
+            cout<<m<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
