2fc242ea304cfc7918ee228b99f98a8112db4ac7
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2022-11-01 15:51:44 +0530

    Committing files

---
+++ CodeChef/C++14/CBSPEED/65923177.cpp
@@ -0,0 +1,107 @@
+
+// Created by: G Gautham Krishna
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// shortforms
+
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define all(x) (x).begin(),(x).end()
+#define srt(v) sort(v.begin(),v.end())
+#define rev(v) reverse(v.begin(),v.end())
+#define lb(v,x) lower_bound(v.begin(),v.end(),x)
+#define ub(v,x) upper_bound(v.begin(),v.end(),x)
+#define cpy(v2,v1) v2.assign(v1.begin(),v1.end())
+#define maxv(a) *max_element(a.begin(), a.end())
+#define minv(a) *min_element(a.begin(), a.end())
+#define ff first
+#define ss second
+#define endl "\n"
+
+//type definitions
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+//constants
+
+const long long int inf = 1e18;
+const int mod = 1000000007;
+#define pi 3.141592653589793238462
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+//graphs
+
+//const int N=1e5+2;
+//std::vector<int> vis(N,0);
+//std::vector<int> adj[N];
+
+// debugger
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(multimap <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+int main() {
+
+
+    //fast io
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    //main code starts here
+    // while(t--){
+        ll x,y;
+        cin>>x>>y;
+        if(y>x){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    // }
+    return 0;
+}
