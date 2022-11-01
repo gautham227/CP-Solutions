934322d6b58788fdf90315121d65e15d68b4092b
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2022-11-01 15:48:57 +0530

    Committing files

---
+++ CodeChef/C++14/OFFICE/69639073.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,m;
+	    cin>>n>>m;
+	    cout<<4*n+m<<endl;
+	}
+	return 0;
+}
