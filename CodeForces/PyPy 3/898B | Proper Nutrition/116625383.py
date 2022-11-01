6962fd429bb8240640d92b646fc13033fa56b642
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2022-11-01 17:09:18 +0530

    Committing files

---
+++ CodeForces/PyPy 3/898B | Proper Nutrition/116625383.py
@@ -0,0 +1,27 @@
+a=int(input())
+b=int(input())
+c=int(input())
+k=0
+if a%b==0 or a%c==0:
+    print("YES")
+    if a%b==0:
+        print(a//b,0)
+    else:
+        print(0,a//c)
+elif a%2!=0 and (b%2==0 and c%2==0):
+    print("NO")
+else:
+    c1=0
+    c2=0
+    while a>b-1:
+        a=a-b
+        c1=c1+1
+        if a%c==0:
+            c2=a//c
+            k=1
+            break
+    if k==1:
+        print("YES")
+        print(c1,c2)
+    else:
+        print("NO")
