d4d608a893e36e823613ef393a468ccabc79f8fb
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2022-11-01 16:41:48 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1676G | White-Black Balanced Subtrees/156696083.cpp
@@ -0,0 +1,134 @@
+// Created by: G Gautham Krishna
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+// shortforms
+ 
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define all(x) (x).begin(),(x).end()
+#define srt(v) sort(v.begin(),v.end())
+#define rev(v) reverse(v.begin(),v.end())
+#define lb(v,x) lower_bound(v.begin(),v.end(),x)
+#define ub(v,x) upper_bound(v.begin(),v.end(),x)
+#define cpy(v2,v1) v2.assign(v1.begin(),v1.end())
+#define maxv(a) *max_element(a.begin(), a.end())
+#define minv(a) *min_element(a.begin(), a.end())
+#define ff first
+#define ss second
+ 
+//type definitions
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+ 
+//constants
+ 
+const long long int inf = 1e18;
+const int mod = 1000000007;
+#define pi 3.141592653589793238462
+ 
+//graphs
+ 
+// const int N=1e5+2;
+// std::vector<int> vis(N,0);
+// std::vector<int> adj[N];
+ 
+// debugger
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+ 
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(multimap <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+ 
+void dfs(int u, vector<vector<int> > & adj, vector<pii> &cnt, string &s, vector<int> & vis) {
+    if(vis[u]==1)return;
+    vis[u]=1;
+    if(adj[u].size()==0){
+        if(s[u-1]=='B'){
+            cnt[u].ff++;
+        }
+        else{
+            cnt[u].ss++;
+        }
+        return;
+    }
+    if(s[u-1]=='B'){
+        cnt[u].ff++;
+    }
+    else{
+        cnt[u].ss++;
+    }
+    for(auto c: adj[u]){
+        dfs(c, adj, cnt, s, vis);
+        cnt[u].ff+=cnt[c].ff;
+        cnt[u].ss+=cnt[c].ss;
+    }
+    return;
+}
+ 
+int main() {
+ 
+    //fast io
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    //main code starts here
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        // vector<int> p;
+        // p.pb(0);
+        // p.pb(1);
+        vector<vector<int> >adj(n+1);
+        vector<pair<int,int> >cnt(n+1);
+        vector<int> vis(n+1, 0);
+        for(int i=2;i<=n;i++){
+            int k;
+            cin>>k;
+            // p.pb(k);
+            adj[k].pb(i);
+        }
+        string s;
+        cin>>s;
+        dfs(1, adj, cnt, s, vis);
+        ll ans=0;
+        for(int i=0;i<cnt.size();i++){
+            if(cnt[i].ff!=0 && cnt[i].ff==cnt[i].ss){
+                ans++;;
+            }
+        }
+        // debug(cnt)
+        cout<<ans<<endl;
+    }
+    return 0;
+}
