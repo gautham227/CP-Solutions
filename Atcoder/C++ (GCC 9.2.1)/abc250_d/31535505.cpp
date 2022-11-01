8a1e99ebf50fb502824d85123ad5d3223dfe6cfc
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2022-11-01 15:40:33 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc250_d/31535505.cpp
@@ -0,0 +1,133 @@
+// Created by: G Gautham Krishna
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// shortforms
+
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define all(x) (x).begin(),(x).end()
+#define srt(v) sort(v.begin(),v.end())
+#define rev(v) reverse(v.begin(),v.end())
+#define lb(v,x) lower_bound(v.begin(),v.end(),x)
+#define ub(v,x) upper_bound(v.begin(),v.end(),x)
+#define cpy(v2,v1) v2.assign(v1.begin(),v1.end())
+#define maxv(a) *max_element(a.begin(), a.end())
+#define minv(a) *min_element(a.begin(), a.end())
+#define ff first
+#define ss second
+
+//type definitions
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+//constants
+
+const long long int inf = 1e18;
+const int mod = 1000000007;
+#define pi 3.141592653589793238462
+
+//graphs
+
+//const int N=1e5+2;
+//std::vector<int> vis(N,0);
+//std::vector<int> adj[N];
+
+// debugger
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(multimap <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+vector<char> isPrime(1e6+10, true);
+
+vector<char> segmentedSieve(long long L, long long R) {
+    // generate all primes up to sqrt(R)
+    long long lim = sqrt(R);
+    vector<char> mark(lim + 1, false);
+    vector<long long> primes;
+    for (long long i = 2; i <= lim; ++i) {
+        if (!mark[i]) {
+            primes.emplace_back(i);
+            for (long long j = i * i; j <= lim; j += i)
+                mark[j] = true;
+        }
+    }
+
+    for (long long i : primes)
+        for (long long j = max(i * i, (L + i - 1) / i * i); j <= R; j += i)
+            isPrime[j - L] = false;
+    if (L == 1)
+        isPrime[0] = false;
+    return isPrime;
+}
+
+int main() {
+
+    //fast io
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    //main code starts here
+    ll n;
+    cin >> n;
+    segmentedSieve(1, 1000000+1);
+    vector<int> primes;
+    for(int i=0;i<isPrime.size();i++) {
+        if(isPrime[i]) {
+            primes.pb(i+1);
+        }
+    }
+    ll ans=0;
+    // for(int i=0;i<10;i++){
+    //     cout<<primes[i]<<" ";
+    // }
+    // cout<<endl;
+    for(int i=1;i<primes.size();i++) {
+        ll x=pow(primes[i],3);
+        // debug(x)
+        if(x>n){
+            break;
+        }
+        ll u=min(1LL*n/x,1LL*primes[i]-1);
+        // debug(u)
+        auto l=lower_bound(primes.begin(),primes.end(),u);
+        int len=l-primes.begin()+1;
+        // debug(len)
+        if(*l>u){
+            len--;
+        }
+        ans+=len;
+    }
+    cout << ans << endl;
+    return 0;
+}
