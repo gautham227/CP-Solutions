96205c83f3e0fad9236c1077ea30cf1b2e301239
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2022-11-01 15:54:09 +0530

    Committing files

---
+++ CodeChef/C++17/XORGAND/64364654.cpp
@@ -0,0 +1,178 @@
+// Created by: G Gautham Krishna
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// shortforms
+
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define all(x) (x).begin(),(x).end()
+#define srt(v) sort(v.begin(),v.end())
+#define rev(v) reverse(v.begin(),v.end())
+#define lb(v,x) lower_bound(v.begin(),v.end(),x)
+#define ub(v,x) upper_bound(v.begin(),v.end(),x)
+#define cpy(v2,v1) v2.assign(v1.begin(),v1.end())
+#define maxv(a) *max_element(a.begin(), a.end())
+#define minv(a) *min_element(a.begin(), a.end())
+#define ff first
+#define ss second
+
+//type definitions
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+//constants
+
+const long long int inf = 1e18;
+const int mod = 1000000007;
+#define pi 3.141592653589793238462
+
+//graphs
+
+//const int N=1e5+2;
+//std::vector<int> vis(N,0);
+//std::vector<int> adj[N];
+
+// debugger
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(multimap <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+int findlen(int no){
+    int len=0;
+    while(no){
+        no/=2;
+        len++;
+    }
+    return len;
+}
+
+class segtree
+{
+    vector<vector<int> > seg;
+public:
+
+    segtree(int n){
+        seg.resize(4*n+1);
+        for(int i=0;i<4*n+1;i++){
+            seg[i].resize(35,0);
+        }
+    }
+
+    void build(int ind, int low, int high, vector<int> &arr){
+        if(low==high){
+            seg[ind][findlen(arr[low])]++;
+            return;
+        }
+        int mid=low+(high-low)/2;
+        build(2*ind+1,low,mid,arr);
+        build(2*ind+2,mid+1,high,arr);
+        for(int i=0;i<seg[ind].size();i++){
+            seg[ind][i]=seg[2*ind+1][i]+seg[2*ind+2][i];
+        }
+        return;
+    }
+
+    void printseg(){
+        for(int i=0;i<seg.size();i++){
+            cout<<i<<endl;
+            for(int j=0;j<seg[i].size();j++){
+                cout<<seg[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+
+    int query(int ind, int low, int high, int l, int r, int x){
+        if(low>r || high<l)return 0;
+        int xl=findlen(x);
+        // debug(xl)
+        if(l<=low && r>=high){
+            int ans=0;
+            for(int i=0;i<seg[ind].size();i++){
+                if(i!=xl)ans+=seg[ind][i];
+            }
+            return ans;
+        }
+        int mid=low+ (high-low)/2;
+        int left=query(2*ind+1, low, mid, l, r, x);
+        int right=query(2*ind+2, mid+1, high, l, r, x);
+        return left+right;
+    }
+
+    // void update(int ind, int low, int high, int i, int val){
+    //     if(low==high){
+    //         seg[ind]=val;
+    //         return;
+    //     }
+    //     int mid=low+(high-low)/2;
+    //     if(i<=mid){
+    //         update(2*ind+1, low, mid, i, val);
+    //     }
+    //     else{
+    //         update(2*ind+2, mid+1, high, i, val);
+    //     }
+    //     seg[ind]=min(seg[2*ind+1], seg[2*ind+2]);
+    // }
+        
+};
+
+int main() {
+
+    //fast io
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    //main code starts here
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        segtree tree(n);
+        tree.build(0,0,n-1,arr);
+        int q;
+        cin>>q;
+        // tree.printseg();
+        while(q--){
+            int l,r,x;
+            cin>>l>>r>>x;
+            cout<<tree.query(0,0,n-1,l-1,r-1,x)<<endl;
+        }
+    }
+    return 0;
+}
