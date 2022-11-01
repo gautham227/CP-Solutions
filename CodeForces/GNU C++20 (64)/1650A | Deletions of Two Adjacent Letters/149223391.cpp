6e29e05add1a902bf72fd156325bf4f4b36949dc
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2022-11-01 16:46:25 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1650A | Deletions of Two Adjacent Letters/149223391.cpp
@@ -0,0 +1,61 @@
+// Created by: G Gautham Krishna
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+// shortforms
+ 
+#define pb push_back
+#define pf push_front
+#define all(x) (x).begin(),(x).end()
+#define srt(v) sort(v.begin(),v.end())
+#define rev(v) reverse(v.begin(),v.end())
+#define maxv(a) *max_element(a.begin(), a.end())
+#define minv(a) *min_element(a.begin(), a.end())
+#define ff first
+#define ss second
+ 
+//type definitions
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+ 
+ 
+int main() {
+ 
+    //fast io
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    //main code starts here
+    int t;
+    cin>>t;
+    while(t--){
+        string s1;
+        char c;
+        cin>>s1;
+        cin>>c;
+        if(find(s1.begin(),s1.end(),c)!=s1.end()){
+            int f=0;
+            for(int i=0;i<s1.size();i++){
+                if(s1[i]==c && i%2==0){
+                    f=1;
+                    break;
+                }
+            }
+            if(f==1){
+                cout<<"YES"<<endl;
+            }
+            else{
+                cout<<"NO"<<endl;
+            }
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}
