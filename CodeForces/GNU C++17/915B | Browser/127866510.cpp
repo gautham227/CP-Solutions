cb66ae96b1c1e03b352a54bbf8624da0fbf65834
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2022-11-01 16:59:55 +0530

    Committing files

---
+++ CodeForces/GNU C++17/915B | Browser/127866510.cpp
@@ -0,0 +1,93 @@
+// Created by: G Gautham Krishna
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+// shortforms
+ 
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define all(x) (x).begin(),(x).end()
+#define srt(v) sort(v.begin(),v.end())
+#define cpy(v2,v1) v2.assign(v1.begin(),v1.end())
+#define maxv(a) *max_element(a.begin(), a.end())
+#define minv(a) *min_element(a.begin(), a.end())
+#define ff first
+#define ss second
+ 
+//type definitions
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+ 
+//constants
+ 
+const long long int inf = 1e18;
+const int mod = 1000000007;
+#define pi 3.141592653589793238462
+ 
+// debugger
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+ 
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(multimap <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+ 
+int main() {
+#ifndef ONLINE_JUDGE
+freopen("inputf.in", "r", stdin);
+freopen("outputf.in", "w", stdout);
+freopen("Error.txt", "w", stderr);
+#endif
+ 
+  //fast io
+  ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+  //main code starts here
+  int n,pos,l,r;
+  cin>>n>>pos>>l>>r;
+  if ((l==1 && r==n)||n==1){
+    cout<<0<<endl;
+  }
+  else if(l==1){
+    int t=0;
+    t=t+abs(r-pos)+1;
+    cout<<t<<endl;
+  }
+  else if(r==n){
+    int t=0;
+    t=t+abs(pos-l)+1;
+    cout<<t<<endl;
+  }
+  else{
+    int m=min(abs(pos-l),abs(pos-r));
+    int t=m+r-l+2;
+    cout<<t<<endl;
+  }
+  return 0;
+}
