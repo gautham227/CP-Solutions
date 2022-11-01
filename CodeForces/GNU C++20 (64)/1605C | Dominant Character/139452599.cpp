555cf303f2d35aadc46a6f59a7d1cb96aa77a0ba
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2022-11-01 16:56:07 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1605C | Dominant Character/139452599.cpp
@@ -0,0 +1,165 @@
+// Created by: G Gautham Krishna
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+// shortforms
+ 
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define all(x) (x).begin(),(x).end()
+#define srt(v) sort(v.begin(),v.end())
+#define rev(v) reverse(v.begin(),v.end())
+#define lb(v,x) lower_bound(v.begin(),v.end(),x)
+#define ub(v,x) upper_bound(v.begin(),v.end(),x)
+#define cpy(v2,v1) v2.assign(v1.begin(),v1.end())
+#define maxv(a) *max_element(a.begin(), a.end())
+#define minv(a) *min_element(a.begin(), a.end())
+#define ff first
+#define ss second
+ 
+//type definitions
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+ 
+//constants
+ 
+const long long int inf = 1e18;
+const int mod = 1000000007;
+#define pi 3.141592653589793238462
+ 
+//graphs
+ 
+//const int N=1e5+2;
+//std::vector<int> vis(N,0);
+//std::vector<int> adj[N];
+ 
+// debugger
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+ 
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(multimap <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+ 
+void computeLPSArray(string pat, int M,
+                                 int lps[])
+{
+    int len = 0;
+    int i = 1;
+    lps[0] = 0; // lps[0] is always 0
+    while (i < M)
+    {
+        if (pat[i] == pat[len])
+        {
+            len++;
+            lps[i] = len;
+            i++;
+        }
+        else // (pat[i] != pat[len])
+        {
+            if (len != 0)
+            {
+                len = lps[len - 1];
+            }
+            else // if (len == 0)
+            {
+                lps[i] = len;
+                i++;
+            }
+        }
+    }
+}
+ 
+int KMPSearch(string pat, string txt)
+{
+    int M = pat.length();
+    int N = txt.length();
+    int lps[M];
+    int j = 0; // index for pat[]
+    computeLPSArray(pat, M, lps);
+ 
+    int i = 0; // index for txt[]
+    int res = 0;
+    int next_i = 0;
+ 
+    while (i < N)
+    {
+        if (pat[j] == txt[i])
+        {
+            j++;
+            i++;
+        }
+        if (j == M)
+        {
+            j = lps[j - 1];
+            res++;
+        }
+        else if (i < N && pat[j] != txt[i])
+        {
+            if (j != 0)
+                j = lps[j - 1];
+            else
+                i = i + 1;
+        }
+    }
+    return res;
+}
+ 
+int main() {
+ 
+    //fast io
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    //main code starts here
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        if(KMPSearch("aa",s)!=0){
+            cout<<2<<endl;
+        }
+        else if(KMPSearch("aba",s)!=0 || KMPSearch("aca",s)!=0){
+            cout<<3<<endl;
+        }
+        else if(KMPSearch("abca",s)!=0 || KMPSearch("acba",s)!=0){
+            cout<<4<<endl;
+        }
+        else if(KMPSearch("abbacca",s)!=0 || KMPSearch("accabba",s)){
+            cout<<7<<endl;
+        }
+        else{
+            cout<<-1<<endl;
+        }
+    }
+    
+    return 0;
+}
