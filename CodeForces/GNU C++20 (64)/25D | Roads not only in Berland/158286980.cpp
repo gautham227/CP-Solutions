0ccfd2e4e69ac90ba467e5356956756e99761099
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2022-11-01 16:38:53 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/25D | Roads not only in Berland/158286980.cpp
@@ -0,0 +1,137 @@
+// Created by: G Gautham Krishna
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+// shortforms
+ 
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define all(x) (x).begin(),(x).end()
+#define srt(v) sort(v.begin(),v.end())
+#define rev(v) reverse(v.begin(),v.end())
+#define lb(v,x) lower_bound(v.begin(),v.end(),x)
+#define ub(v,x) upper_bound(v.begin(),v.end(),x)
+#define cpy(v2,v1) v2.assign(v1.begin(),v1.end())
+#define maxv(a) *max_element(a.begin(), a.end())
+#define minv(a) *min_element(a.begin(), a.end())
+#define ff first
+#define ss second
+ 
+//type definitions
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+ 
+//constants
+ 
+const long long int inf = 1e18;
+const int mod = 1000000007;
+#define pi 3.141592653589793238462
+ 
+//graphs
+ 
+//const int N=1e5+2;
+//std::vector<int> vis(N,0);
+//std::vector<int> adj[N];
+ 
+// debugger
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+ 
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(multimap <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+ 
+class DSU{
+    vector<int> par;
+    vector<int> size;
+    public:
+    DSU(int n){
+        for(int i=0;i<=n;i++){
+            par.push_back(i);
+        }
+        size.assign(n+1,1);
+    }
+ 
+    int find(int x){
+        if(par[x]==x)return x;
+        return par[x]=find(par[x]);
+    }
+ 
+    void Union(int u, int v){
+        int pu=find(u);
+        int pv=find(v);
+        if(pu==pv)return;
+ 
+        if(size[pu]>size[pv]){
+            par[pv]=pu;
+            size[pu]+=size[pv];
+        }
+        else{
+            par[pu]=pv;
+            size[pv]+=size[pu];
+        }
+        return;
+    }
+};
+ 
+int main() {
+ 
+    //fast io
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    //main code starts here
+    int n;
+    cin>>n;
+    DSU ds(n);
+    vector<pii> ex;
+    for(int i=0;i<n-1;i++){
+        int a,b;
+        cin>>a>>b;
+        if(ds.find(a)==ds.find(b)){
+            ex.pb({a,b});
+        }
+        else{
+            ds.Union(a,b);
+        }
+    }
+    vector<int> ne;
+    for(int i=1;i<=n;i++){
+        if(ds.find(i)==i){
+            ne.pb(i);
+        }
+    }
+    int cnt=ne.size()-1;
+    cout<<cnt<<"\n";
+    for(int i=0;i<cnt;i++){
+        cout<<ex[i].first<<" "<<ex[i].second<<" "<<ne[i]<<" "<<ne[i+1]<<endl;
+    }
+    return 0;
+}
