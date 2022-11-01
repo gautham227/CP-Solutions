e21c3c46c447d54e7b621c85adf14364ba5d027d
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2022-11-01 17:04:39 +0530

    Committing files

---
+++ CodeForces/Python 3/82A | Double Cola/120504519.py
@@ -0,0 +1,13 @@
+#coded by gautham on 25/6
+import math
+n=int(input())
+if n==1:
+    print("Sheldon")
+else:
+    l=["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"]
+    k=1
+    while n>k*5:
+        n=n-5*k
+        k=k*2
+    s=math.ceil(n/k)
+    print(l[s-1])
