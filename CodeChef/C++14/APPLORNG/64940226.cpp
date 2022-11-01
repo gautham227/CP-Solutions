af3236cb44335b329842042e46d4e6fae645ceb6
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2022-11-01 15:53:14 +0530

    Committing files

---
+++ CodeChef/C++14/APPLORNG/64940226.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	cin>>x;
+	int a,b;
+	cin>>a>>b;
+	if(x>=a+b){
+	    cout<<"YES"<<endl;
+	}
+	else{
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
