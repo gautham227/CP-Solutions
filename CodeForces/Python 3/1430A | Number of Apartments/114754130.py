3827abdc54d9caa7efd981d810160ac1f078e093
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2022-11-01 17:11:39 +0530

    Committing files

---
+++ CodeForces/Python 3/1430A | Number of Apartments/114754130.py
@@ -0,0 +1,12 @@
+t=int(input())
+for i in  range(0,t):
+    n=int(input())
+    if n==1 or n==2 or n==4:
+        print(-1)
+        continue
+    elif n%3==0:
+        print(n//3,0,0)
+    elif (n-5)%3==0:
+        print((n-5)//3,1,0)
+    else:
+        print((n-7)//3,0,1)
