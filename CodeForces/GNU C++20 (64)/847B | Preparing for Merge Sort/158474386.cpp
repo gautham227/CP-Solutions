f927764b87ea6c86059c53746ac3dfd4802074d6
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2022-11-01 16:37:28 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/847B | Preparing for Merge Sort/158474386.cpp
@@ -0,0 +1,133 @@
+// Created by: G Gautham Krishna
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+// shortforms
+ 
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define all(x) (x).begin(),(x).end()
+#define srt(v) sort(v.begin(),v.end())
+#define rev(v) reverse(v.begin(),v.end())
+#define lb(v,x) lower_bound(v.begin(),v.end(),x)
+#define ub(v,x) upper_bound(v.begin(),v.end(),x)
+#define cpy(v2,v1) v2.assign(v1.begin(),v1.end())
+#define maxv(a) *max_element(a.begin(), a.end())
+#define minv(a) *min_element(a.begin(), a.end())
+#define ff first
+#define ss second
+ 
+//type definitions
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+ 
+//constants
+ 
+const long long int inf = 1e18;
+const int mod = 1000000007;
+#define pi 3.141592653589793238462
+ 
+//graphs
+ 
+//const int N=1e5+2;
+//std::vector<int> vis(N,0);
+//std::vector<int> adj[N];
+ 
+// debugger
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+ 
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(multimap <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+ 
+// first value from left less than k
+// FFFFTTTTTTT
+ 
+ll ultra_lowerbound(vector<ll>& v, ll k){
+    ll lo=0, hi=v.size()-1;
+    ll mid;
+    while(hi-lo>1){
+        mid=(lo+hi)/2;
+        if(v[mid]<k){
+            hi=mid;
+        }
+        else{
+            lo=mid+1;
+        }
+    }
+    if(v[lo]<k){
+        return lo;
+    }
+    else{
+        return hi;
+    }
+ 
+}
+ 
+int main() {
+ 
+    //fast io
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    //main code starts here
+    ll n;
+    cin>>n;
+    vector<ll> v;
+    for(int i=0;i<n;i++){
+        ll k;
+        cin>>k;
+        v.pb(k);
+    }
+ 
+    vector<vector<ll> >ans;
+    vector<ll> lele;
+    lele.pb(v[0]);
+    ans.pb({v[0]});
+    for(int i=1;i<n;i++){
+        if(v[i]<=lele[lele.size()-1]){
+            ans.pb({v[i]});
+            lele.pb(v[i]);
+        }
+        else{
+            ll ind=ultra_lowerbound(lele,v[i]);
+            ans[ind].pb(v[i]);
+            lele[ind]=v[i];
+        }
+    }
+    for(int i=0;i<ans.size();i++){
+        for(int j=0;j<ans[i].size();j++){
+            cout<<ans[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
