cb9e04f3eda7c23d2ef92d36a95ecbc9c921c8cb
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2022-11-01 16:59:04 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1574C | Slay the Dragon/130231478.cpp
@@ -0,0 +1,123 @@
+    // Created by: G Gautham Krishna
+    #include <bits/stdc++.h>
+     
+    using namespace std;
+     
+    // shortforms
+     
+    #define pb push_back
+    #define pf push_front
+    #define ppb pop_back
+    #define ppf pop_front
+    #define all(x) (x).begin(),(x).end()
+    #define srt(v) sort(v.begin(),v.end())
+    #define cpy(v2,v1) v2.assign(v1.begin(),v1.end())
+    #define maxv(a) *max_element(a.begin(), a.end())
+    #define minv(a) *min_element(a.begin(), a.end())
+    #define ff first
+    #define ss second
+     
+    //type definitions
+     
+    typedef long long ll;
+    typedef unsigned long long ull;
+    typedef long double lld;
+     
+    //constants
+     
+    const long long int inf = 1e18;
+    const int mod = 1000000007;
+    #define pi 3.141592653589793238462
+     
+    // debugger
+     
+    #ifndef ONLINE_JUDGE
+    #define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+    #else
+    #define debug(x)
+    #endif
+     
+    void _print(ll t) {cerr << t;}
+    void _print(int t) {cerr << t;}
+    void _print(string t) {cerr << t;}
+    void _print(char t) {cerr << t;}
+    void _print(lld t) {cerr << t;}
+    void _print(double t) {cerr << t;}
+    void _print(ull t) {cerr << t;}
+     
+    template <class T, class V> void _print(pair <T, V> p);
+    template <class T> void _print(vector <T> v);
+    template <class T> void _print(set <T> v);
+    template <class T, class V> void _print(map <T, V> v);
+    template <class T> void _print(multiset <T> v);
+    template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+    template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+    template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+    template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+    template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+    template <class T, class V> void _print(multimap <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+     
+    int main() {
+    #ifndef ONLINE_JUDGE
+    freopen("inputf.in", "r", stdin);
+    freopen("outputf.in", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+    #endif
+     
+        //fast io
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+     
+        //main code starts here
+        ll n;
+        cin>>n;
+        debug(n)
+        std::vector<ll> v;
+        for (ll i = 0; i < n; ++i)
+        {
+            ll k;
+            cin>>k;
+            v.pb(k);
+        }
+        srt(v);
+        debug(v)
+        ll s=accumulate(all(v),ll(0));
+        ll t;
+        cin>>t;
+        while(t--){
+            ll a,b;
+            debug(a)
+            ll mon=0;
+            ll sum=s;
+            debug(sum)
+            cin>>a>>b;
+            if (a>v[n-1]){
+                sum=sum-v[n-1];
+                cout<<(a-v[n-1])+max(b-sum,ll(0))<<endl;
+                continue;
+            }
+            else{
+                auto l=lower_bound(all(v),a);
+                if(*l==a){
+                    cout<<max(ll(0),b-sum+*l)<<endl;
+                    continue;
+                }
+                else if(sum-*l>=b){
+                    cout<<0<<endl;
+                }
+                else{
+                    ll a1=inf;
+                    if (l!=v.begin()){
+                        auto p=prev(l,1);
+                        debug(*p)
+                        debug(*l-a)
+                        debug(a-*p<=*l-a)
+                        a1=a-*p+max(ll(0),b-sum+*p);
+                    }   
+                    ll ans=min(a1,max(ll(0),b-sum+*l));
+                    cout<<ans<<endl;
+                }
+            }
+        }
+        return 0;
+    }
