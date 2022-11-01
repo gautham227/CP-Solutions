a6f85cb3ad4557e53180d887126e2f652e15594b
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2022-11-01 16:49:36 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1133A | Middle of the Contest/143121656.cpp
@@ -0,0 +1,102 @@
+// Created by: G Gautham Krishna
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+// shortforms
+ 
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define all(x) (x).begin(),(x).end()
+#define srt(v) sort(v.begin(),v.end())
+#define rev(v) reverse(v.begin(),v.end())
+#define lb(v,x) lower_bound(v.begin(),v.end(),x)
+#define ub(v,x) upper_bound(v.begin(),v.end(),x)
+#define cpy(v2,v1) v2.assign(v1.begin(),v1.end())
+#define maxv(a) *max_element(a.begin(), a.end())
+#define minv(a) *min_element(a.begin(), a.end())
+#define ff first
+#define ss second
+ 
+//type definitions
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+ 
+//constants
+ 
+const long long int inf = 1e18;
+const int mod = 1000000007;
+#define pi 3.141592653589793238462
+ 
+//graphs
+ 
+//const int N=1e5+2;
+//std::vector<int> vis(N,0);
+//std::vector<int> adj[N];
+ 
+// debugger
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+ 
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(multimap <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+ 
+int main() {
+  //fast io
+  ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+  //main code starts here
+  string t1,t2;
+  cin>>t1;
+  cin>>t2;
+  int h1=(t1[0]-'0')*10+(t1[1]-'0');
+  int m1=(t1[3]-'0')*10+(t1[4]-'0');
+  int h2=(t2[0]-'0')*10+(t2[1]-'0');
+  int m2=(t2[3]-'0')*10+(t2[4]-'0');
+  int ans=(h2*60+m2+h1*60+m1)/2;
+  int ansh=ans/60;
+  int ansm=ans%60;
+  string h="",m="";
+  if(ansh<10){
+      h='0'+to_string(ansh);
+  }
+  else{
+      h=to_string(ansh);
+  }
+  if(ansm<10){
+      m='0'+to_string(ansm);
+  }
+  else{
+      m=to_string(ansm);
+  }
+  cout<<h<<":"<<m<<endl;
+  return 0;
+}
